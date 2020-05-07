dd741aa8139fe5b29a48e3378a7805130f9a573e
Tiffany C <tiffany.cissel@gmail.com>
2020-05-07 14:11:34 -0400

    Re-configure loop logic on front-page; remove parent theme code chunk

--- front-page.php
+++ front-page.php
@@ -35,64 +35,66 @@ get_header('front'); ?>
         <section class="home-recent-posts">
             <h2>Happenings</h2>
 
-        <?php // Show the selected frontpage content.
-        
-        //my "loop"
+            <?php // Show the selected frontpage content.
+            
+            //my "loop"
 
-        $recent_post_args = array('numberposts'=> 3);
-        $recent_posts = get_posts($recent_post_args);
+                $front_page_query_args = array(
+                    'posts_per_page' => 3,
+                );
+                $front_page_posts = new WP_Query( $front_page_query_args );
+                $front_page_post_ids = array();
+                
+                if( $front_page_posts->have_posts() ):
+                    while( $front_page_posts->have_posts() ):
+                        $front_page_posts->the_post();
+                        array_push( $front_page_post_ids, get_the_ID() );
+                    endwhile;
+                endif;
+                wp_reset_postdata();
 
-        $latest_post = $recent_posts[0];
-        $latest_post_ID = $latest_post->ID;
-        $latest_post_title = $latest_post->post_title;
-        $latest_post_link = get_permalink($latest_post_ID);
+                $latest_post = get_post( $front_page_post_ids[0] );
+                $latest_post_ID = $latest_post->ID;
+                $latest_post_title = $latest_post->post_title;
+                $latest_post_link = get_permalink($latest_post_ID);
+            ?>
 
-        echo '<article id="post-'.$latest_post_ID.'"';
-        post_class('recent-post-1', $latest_post_ID);
-        echo '><h3><a href="'.$latest_post_link.'">'.$latest_post_title.'</a></h3><figure>';
-        echo get_the_post_thumbnail($latest_post_ID, 'full');
-        ?>
-        </figure>
-        <p>
+            <article id="post-<?php echo $latest_post_ID; ?>"
+            <?php echo post_class('recent-post-1', $latest_post_ID); ?>>
+                <h3>
+                    <a href="<?php echo $latest_post_link; ?>"><?php echo $latest_post_title; ?></a>
+                </h3>
+                <figure>
+                <?php echo get_the_post_thumbnail($latest_post_ID, 'full'); ?>      
+                </figure>
+                <p>
+                <?php if(empty($latest_post->post_excerpt)):
+                    echo wp_trim_words($latest_post->post_content, 20).'<a class="scot-read-more-link" href="'.$latest_post_link.'">Read more</a>';
+                else:
+                    echo $latest_post->post_excerpt;
+                endif; ?>
+                </p>
+            </article>
 
-        <?php if(empty($latest_post->post_excerpt)){
-            echo wp_trim_words($latest_post->post_content, 20).'<a class="scot-read-more-link" href="'.$latest_post_link.'">Read more</a>';
-        } else {
-            echo $latest_post->post_excerpt;
-        }
-        ?>
-        </p>
-        </article>
-
-        <div class="older-articles">
-        
-        <?php
-        $long_title = false;
-
-        for($i = 1; $i<sizeof($recent_posts); $i++){
-            if(!$long_title){
-                if(str_word_count($recent_posts[$i]->post_title)>5){
-                    $long_title = true;
-                }
-            }            
-        }        
-
-        for($i = 1; $i<sizeof($recent_posts); $i++) {
-            $rpost_title = $recent_posts[$i]->post_title;
-            $rpost_link = get_permalink($recent_posts[$i]->ID);
-            echo '<article id="post-'.$recent_posts[$i]->ID.'"';
-            post_class('recent-post-'.($i+1), $recent_posts[$i]->ID);
-            if(!$long_title){
-                echo '><h3><a href="'.$rpost_link.'">'.$rpost_title.'</a></h3><figure>';
-            } else {
-                echo '><h3 class="recent-post-long-title"><a href="'.$rpost_link.'">'.$rpost_title.'</a></h3><figure>';
-            }            
-            echo get_the_post_thumbnail($recent_posts[$i]->ID,'medium');
-            echo '</figure></article>';  
-        }
-
-        ?>
-        </div>
+            <div class="older-articles">
+            
+            <?php
+            for( $i = 1; $i < sizeof( $front_page_post_ids ); $i++ ):
+                $current_post = get_post( $front_page_post_ids[$i] );
+                $rpost_title = $current_post->post_title;
+                $rpost_link = get_permalink($current_post->ID);
+                $h3_class = str_word_count($rpost_title) > 5 ? ' class="recent-post-long-title"' : '';
+                ?>
+                <article id="post-<?php echo $current_post->ID;?>" <?php echo post_class('recent-post-'.($i+1), $current_post->ID); ?>>
+                    <h3<?php echo $h3_class; ?>>
+                        <a href="<?php echo $rpost_link; ?>"><?php echo $rpost_title; ?></a>
+                    </h3>
+                    <figure>          
+                        <?php echo get_the_post_thumbnail($current_post->ID,'medium'); ?>
+                    </figure>
+                </article>
+            <?php endfor; ?>
+            </div>
         </section>
 
         <section class="home-action">
@@ -103,27 +105,6 @@ get_header('front'); ?>
             <div><a href="events/" role="button">Attend an Event</a></div>
         </section>
 
-        <?php
-        // Get each of our panels and show the post data.
-        if ( 0 !== twentyseventeen_panel_count() || is_customize_preview() ) : // If we have pages to show.
-
-            /**
-             * Filter number of front page sections in Twenty Seventeen.
-             *
-             * @since Twenty Seventeen 1.0
-             *
-             * @param int $num_sections Number of front page sections.
-             */
-            $num_sections = apply_filters( 'twentyseventeen_front_page_sections', 4 );
-            global $twentyseventeencounter;
-
-            // Create a setting and control for each of the sections available in the theme.
-            for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
-                $twentyseventeencounter = $i;
-                twentyseventeen_front_page_section( null, $i );
-            }
-
-    endif; // The if ( 0 !== twentyseventeen_panel_count() ) ends here. ?>
     </main><!-- #main -->
 </div><!-- #primary -->
 
